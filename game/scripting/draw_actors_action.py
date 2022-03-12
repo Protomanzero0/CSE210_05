56385d95e754613bf4ea69b3f98b1c11150340ef
Protomanzero0 <protomanzero01@gmail.com>
2022-03-12 13:36:43 -0700

    No score, no custom GameOver message, but game functions to spec

--- game/scripting/draw_actors_action.py
+++ game/scripting/draw_actors_action.py
@@ -26,8 +26,6 @@ class DrawActorsAction(Action):
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
-        score = cast.get_first_actor("scores")
-        # food = cast.get_first_actor("foods")
         snake1 = cast.get_first_actor("snakes")
         segments1 = snake1.get_segments()
         snake2 = cast.get_second_actor("snakes")
@@ -35,9 +33,7 @@ class DrawActorsAction(Action):
         messages = cast.get_actors("messages")
 
         self._video_service.clear_buffer()
-        # self._video_service.draw_actor(food)
         self._video_service.draw_actors(segments1)
         self._video_service.draw_actors(segments2)
-        self._video_service.draw_actor(score)
         self._video_service.draw_actors(messages, True)
         self._video_service.flush_buffer()
